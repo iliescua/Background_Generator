db25df6c0212251847bdcfa65b1d76ed311cb40b
Andrew Iliescu <iliescuandrew@gmail.com>
2021-07-22 00:04:48 -0500

    Parsed the CSS background code in JS to only display the rgb properties and added a button to copy the CSS background code in full to the clipboard

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ var body = document.getElementById("webpageBody");
 var rgbReport = document.querySelector("h3");
 var firstColor = document.getElementById("firstColor");
 var secondColor = document.getElementById("secondColor");
+var copy_btn = document.querySelector("button");
+var flag = 1;
 
+// Function called to display the current rgb values to screen
 function displayValues(){
     var colorValue = Array.from(body.style.background.toString());
     var colorString = "";
@@ -17,8 +20,11 @@ function displayValues(){
     }
     rgbReport.innerHTML = colorString;
     colorValue = []
+    // Flag used to determine if the user clicks copy before changing color
+    flag = 0;
 }
 
+// Function called to set the gradient colors for the background
 function setGradient() {
     body.style.background = "linear-gradient(to right, " + firstColor.value + ", " + secondColor.value + ")";
     displayValues();
@@ -27,3 +33,14 @@ function setGradient() {
 firstColor.addEventListener("input", setGradient);
 
 secondColor.addEventListener("input", setGradient);
+
+// Button is used to determine if the CSS code for background gets copied to clipboard
+copy_btn.addEventListener("click", function(){
+    // Check to see if user wants to copy CSS background code before changing a color
+    if (flag === 1){
+        copyText = "linear-gradient(to right, rgb(0, 201, 255), rgb(256, 254, 157));";
+    } else {
+        copyText = body.style.background + ";";
+    }
+    navigator.clipboard.writeText(copyText);
+});
