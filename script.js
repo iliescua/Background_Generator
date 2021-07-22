6824ba07ddf6c04b9ca0eeb9b6d25bbb4325da7d
Andrew Iliescu <iliescuandrew@gmail.com>
2021-07-21 22:21:01 -0500

    Created display to allow user to see the rgb values they selected

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 var body = document.getElementById("webpageBody");
-var rgbRepot = document.querySelector("h3");
+var rgbReport = document.querySelector("h3");
 var firstColor = document.getElementById("firstColor");
 var secondColor = document.getElementById("secondColor");
 
+function displayValues(){
+    var colorValue = Array.from(body.style.background.toString());
+    var colorString = "";
+    colorValue.pop();
+    // Shift over to remove all words before the first instance of rgb
+    for (var i = 0; i < 26; i++) {
+        colorValue.shift();
+    }
+    // Convert arry to string manually because built in methods broke format
+    for (var i = 0; i < colorValue.length; i++){
+        colorString += colorValue[i];
+    }
+    rgbReport.innerHTML = colorString;
+    colorValue = []
+}
+
 function setGradient() {
     body.style.background = "linear-gradient(to right, " + firstColor.value + ", " + secondColor.value + ")";
+    displayValues();
 }
 
 firstColor.addEventListener("input", setGradient);
